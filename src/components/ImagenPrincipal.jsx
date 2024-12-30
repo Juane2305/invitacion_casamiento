f8e1ef982648a49ad03d6888ab48cad70840b2a6
Juane Elizondo <juaneelizondo@MacBook-Pro-de-Juane.local>
2024-12-30 16:31:32 -0300

    flecha al inicio corregida

--- src/components/ImagenPrincipal.jsx
+++ src/components/ImagenPrincipal.jsx
@@ -6,7 +6,7 @@ const ImagenPrincipal = () => {
     <div className='h-screen w-full bg-orange-100'>
         <div className='flex md:hidden bg-fondo-save bg-center bg-cover bg-fixed'>
             <div className='flex h-screen w-full justify-center items-end mb-10'>
-                <a href="#info"><img src={arrow} alt="flecha hacia abajo" className='size-12 animate-bounce mb-16'/></a>
+                <a href="#info"><img src={arrow} alt="flecha hacia abajo" className='size-12 animate-bounce mb-20'/></a>
             </div>
         </div>
         <div className='hidden md:flex bg-fondo-save-desk  h-screen w-full bg-center bg-no-repeat bg-cover bg-fixed'>
