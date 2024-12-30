1c74fec8bd7b3c5bce499cf5fb9c7a8535c4387a
Juane Elizondo <juaneelizondo@MacBook-Pro-de-Juane.local>
2024-12-30 16:26:50 -0300

    Texto asistencia centrado

--- src/components/Asistencia.jsx
+++ src/components/Asistencia.jsx
@@ -4,7 +4,7 @@ const Asistencia = () => {
   return (
     <div className='py-20 bg-fondo-tela bg-fixed'>
         <div className='h-full flex flex-col justify-center items-center space-y-2'>
-            <h2 className='text-4xl font-thin'>Confirmación de Asistencia</h2>   
+            <h2 className='text-4xl font-thin text-center'>Confirmación de Asistencia</h2>   
             <a href="https://docs.google.com/forms/d/e/1FAIpQLScPjx4s5j-r4WJZfJjhY3I4oyNiOuK1u3rJv-kaE0is2xjd9Q/viewform?usp=dialog" target='__blank'><button className='bg-principal-dark py-4 px-6 rounded-lg text-white font-bold mt-5'>Confirmar asistencia</button></a>
         </div>
     </div>
