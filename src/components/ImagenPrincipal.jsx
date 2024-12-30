4960193b19a7cefe00558ee58c96636a6cc36b0e
Juane Elizondo <juaneelizondo@MacBook-Pro-de-Juane.local>
2024-12-30 16:23:36 -0300

    Link asistencia corregido

--- src/components/ImagenPrincipal.jsx
+++ src/components/ImagenPrincipal.jsx
@@ -3,8 +3,8 @@ import arrow from '../assets/arrow.svg'
 
 const ImagenPrincipal = () => {
   return (
-    <div className='h-screen bg-orange-100'>
-        <div className='flex md:hidden bg-fondo-save bg-center bg-no-repeat bg-cover bg-fixed'>
+    <div className='h-screen w-full bg-orange-100'>
+        <div className='flex md:hidden bg-fondo-save bg-center bg-cover bg-fixed'>
             <div className='flex h-screen w-full justify-center items-end mb-10'>
                 <a href="#info"><img src={arrow} alt="flecha hacia abajo" className='size-12 animate-bounce mb-10'/></a>
             </div>
