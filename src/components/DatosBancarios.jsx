288726f5e937576e841af20ba925e98267c8c444
Juane Elizondo <juaneelizondo@MacBook-Pro-de-Juane.local>
2024-12-31 17:15:34 -0300

    cambie frase de datos bancarios

--- src/components/DatosBancarios.jsx
+++ src/components/DatosBancarios.jsx
@@ -5,7 +5,7 @@ const DatosBancarios = () => {
   return (
     <div className=" bg-principal-dark mt-20 flex flex-col justify-center items-center py-20">
         <img src={gift} alt="regalo" className="w-40 text-white"/>
-        <p className='text-white text-lg text-center px-5 md:px-0'>Si deseás realizarnos un regalo para nuestra luna de miel...</p>
+        <p className='text-white text-lg text-center px-5 md:px-0'>Si deseás hacernos un regalo te dejamos nuestros datos</p>
         <Modal/>
     </div>
   )
